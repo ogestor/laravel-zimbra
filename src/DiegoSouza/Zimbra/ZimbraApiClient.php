ed6e5b859733c419fd12c08883c8e12231fdcf91
heliopassarelin <49026132+heliopassarelin@users.noreply.github.com>
2022-11-21 14:51:56 -0300

    Update ZimbraApiClient.php

--- src/DiegoSouza/Zimbra/ZimbraApiClient.php
+++ src/DiegoSouza/Zimbra/ZimbraApiClient.php
@@ -21,21 +21,22 @@ class ZimbraApiClient
     protected $logger;
     protected $domain;
 
-    public function __construct($host, $emailDomain, $user, $password, LogManager $logger)
+    public function __construct($host, $emailDomain, $user, $password, LogManager $logger, $doLog = false)
     {
         $this->api = AdminFactory::instance("https://{$host}:7071/service/admin/soap");
         $this->api->auth($user, $password);
 
         $this->domain = $emailDomain;
         $this->logger = $logger;
-
-        $this->api->getClient()->on('before.request', function ($request) {
-            $this->logger->debug("SOAP REQUEST: {$request}");
-        });
-
-        $this->api->getClient()->on('after.request', function ($response) {
-            $this->logger->debug("SOAP RESPONSE: {$response}");
-        });
+        
+        if($doLog) {
+            $this->api->getClient()->on('before.request', function ($request) {
+                $this->logger->debug("SOAP REQUEST: {$request}");
+            });
+            $this->api->getClient()->on('after.request', function ($response) {
+                $this->logger->debug("SOAP RESPONSE: {$response}");
+            });
+        }
     }
 
     public function createDistributionList($name)
